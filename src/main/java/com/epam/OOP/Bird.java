2a2666bfb873e352df8e966920b3bc9145f2be38
maksimumsss <150504703+maksimumsss@users.noreply.github.com>
2024-04-08 09:58:20 +0300

    Update Bird.java

--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -2,8 +2,6 @@ package com.epam.OOP;
 
 public class Bird extends Animal {
   public Bird() {
-    super.color = "blue";
-    super.numberOfPaws = 2;
-    super.hasFur = false;
+    super("blue", 2, false);
   }
 }
