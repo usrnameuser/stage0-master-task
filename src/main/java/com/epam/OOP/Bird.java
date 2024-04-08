d36f381a25d1789d7ed668fac7db195d47cc06f8
maksimumsss <150504703+maksimumsss@users.noreply.github.com>
2024-04-08 10:08:45 +0300

    Update Bird.java

--- src/main/java/com/epam/OOP/Bird.java
+++ src/main/java/com/epam/OOP/Bird.java
@@ -4,7 +4,8 @@ public class Bird extends Animal {
   public Bird() {
     super("blue", 2, false);
   }
-
+  
+  @Override
   public String getDescription() {
     String description;
     String moreDescription = " Moreover, it has 2 wings and can fly.";
