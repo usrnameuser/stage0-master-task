89c9dbb98d0433ebf71dd45a58149c65ada63bb6
maksimumsss <150504703+maksimumsss@users.noreply.github.com>
2024-04-08 08:49:11 +0300

    Update Animal.java

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -4,4 +4,9 @@ public class Animal {
   private String color;
   private int NumberOfPaws;
   private boolean hasFur;
+  public Animal(String color, int NumberOfPaws, boolean hasFur) {
+    this.color = color;
+    this.NumberOfPaws = NumberOfPaws;
+    this.hasFur = hasFur;
+  }  
 }
