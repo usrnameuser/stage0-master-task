1c10c0990d65d3c687e14bd5171844c4ff0932ad
usrnameuser <150504703+usrnameuser@users.noreply.github.com>
2024-07-02 00:23:16 +0300

    Update DigitsSumCalculator.java

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -5,8 +5,11 @@ public class DigitsSumCalculator {
     public void calculateSum(int number) {
         String numberString = String.valueOf(number);
         char[] numberCharArray = numberString.toCharArray();
-
-
+        int digitSum = 0;
+        for(char numberChar : numberCharArray) {
+            digitSum += Character.getNumericValue(numberChar)
+        }
+        System.out.println(digitSum); 
     }
 
 }
