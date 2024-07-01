3759b61534734a171309b81fa31c816426484360
usrnameuser <150504703+usrnameuser@users.noreply.github.com>
2024-07-02 00:12:55 +0300

    Update DigitsSumCalculator.java

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,10 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        String numberString = String.valueOf(number);
+        char[] numberChaeArray = numberString.toCharArray();
+
+        
     }
 
 }
