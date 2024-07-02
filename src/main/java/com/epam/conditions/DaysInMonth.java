459f25ae5b25f2c77d02557aab7f87188732249b
usrnameuser <150504703+usrnameuser@users.noreply.github.com>
2024-07-02 12:30:55 +0300

    Update DaysInMonth.java

--- src/main/java/com/epam/conditions/DaysInMonth.java
+++ src/main/java/com/epam/conditions/DaysInMonth.java
@@ -3,7 +3,13 @@ package com.epam.conditions;
 public class DaysInMonth {
 
     public void printDays(int year, int month) {
-
+       // if(year > 0) {
+            YearMonth yearMonth = YearMonth.of(year, month);
+            int daysInMonth = yearMonthObject.lengthOfMonth();
+            System.out.println(daysInMonth);
+        //} else {
+        //    System.out.println("invalid date");
+        //}
     }
 
 }
