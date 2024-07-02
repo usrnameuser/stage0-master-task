b2dd58d9059bb5486bf1441308203e991769ad34
usrnameuser <150504703+usrnameuser@users.noreply.github.com>
2024-07-02 12:49:56 +0300

    Update IntegerDivider.java

--- src/main/java/com/epam/conditions/IntegerDivider.java
+++ src/main/java/com/epam/conditions/IntegerDivider.java
@@ -3,6 +3,22 @@ package com.epam.conditions;
 public class IntegerDivider {
 
     public void printCompletelyDivided(int dividend, int divider) {
+        if (divider == 0) {
+            System.out.println("division by zero");
+            return;
+        }
+        
+        int divide = dividend / divider;
+        int rebuild = divide * divider;
+        if(rebuild == dividend) {
+            System.out.println("can be divided completely");
+        } else {
+            System.out.println("cannot be divided completely");
+        }
+        
+        
+        
+        
     }
 
 }
